07101c58d4b4021f2c12d0410154c2e546a7fbdb
erikqb3 <erikqb3@gmail.com>
2023-10-17 16:54:06 -0600

    more small progress

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
 	images: {
 		unoptimized: true,
 		domains: [
-			"https://erikqb3.github.io/BiggerOrBetter/",
+			"res.cloudinary.com/",
 			"avatars.githubusercontent.com",
 			"lh3.googleusercontent.com",
 			"via.placeholder.com",
@@ -13,7 +13,7 @@ const nextConfig = {
 	},
 	env: {
 		NEXTAUTH_SECRET: "b51afa1ed38dde0d5d8f218a7fe48e1a",
-		NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "dev-empty",
+		NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "dxkvem7qh", // CHECK!! https://console.cloudinary.com/console/c-0dd466c1e9b508fafe628fad55b216
 		NEXT_CLOUDINARY_PRESET: "vikings",
 	},
 };
